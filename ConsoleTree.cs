7e37da39974c85aaaf1d778f03aea429374cdce8
Yulia Gvardeeva <julia.athen@yandex.ru>
2022-03-24 21:19:59 +0300

    Initial

---
+++ ConsoleTree.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+namespace Tree
+{
+    internal class ConsoleTree
+    {
+        private int Size;
+        public ConsoleTree(int size)
+        {
+            Size = size;
+        }
+
+        public string AsStringOne()
+        {
+            var sb = new StringBuilder("");
+            for (int i = 1; i <= Size; i++)
+            {
+                sb.AppendLine(new String('*', i));
+            }
+            return sb.ToString();
+        }
+        public string AsStringTwo()
+        {
+            Size = Size % 2 == 0 ? Size + 1 : Size;
+            int level = Size / 2 + 1;
+            string format = "{0," + level.ToString() + "}" + "{1," + "-" + level.ToString() + "}";
+            var sb = new StringBuilder("");
+            for (int i = 1; i <= level; i++)
+            {
+                sb.AppendLine(String.Format(format, new String('*', i), new String('*', i - 1)));
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using Tree;
+
+int treeSize = 0;
+if (args.Length > 0)
+{
+    treeSize = int.Parse(args[0]);
+}
+var tree = new ConsoleTree(treeSize);
+Console.WriteLine(tree.AsStringOne());
+Console.WriteLine(tree.AsStringTwo());
+
